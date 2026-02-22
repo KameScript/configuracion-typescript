fa665e951fd7c37da7f2df0a47349be31bf3220b
KameScript <bardock10_90@hotmail.com>
2026-02-22 13:55:36 -0600

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Punto de entrada principal del proyecto TypeScript.
+ * Aquí puedes importar y ejecutar tus ejercicios de aprendizaje.
+ */
+
+import { ejemploTiposBasicos } from './ejemplos/01-tipos-basicos';
+import { ejemploFunciones } from './ejemplos/02-funciones';
+import { ejemploInterfaces } from './ejemplos/03-interfaces';
+
+console.log('🚀 Bienvenido al aprendizaje de TypeScript\n');
+
+console.log('--- 1. Tipos Básicos ---');
+ejemploTiposBasicos();
+
+console.log('\n--- 2. Funciones ---');
+ejemploFunciones();
+
+console.log('\n--- 3. Interfaces ---');
+ejemploInterfaces();
